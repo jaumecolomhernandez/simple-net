570c68452db5143c46ed23677d45a5a61c5086e6
jaume <jaumecolomhernandez@gmail.com>
2019-12-27 05:55:49 +0100

    Added package to contain the code

---
+++ neural_network/network.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Network:
+    def __init__(
+        self,
+        model=None
+    ):  
+        self.layers = model
+    
+
+    def train(self, training_set):
+        pass
+
+    def evaluate(self, evaluation_Set):
+        pass
--- run_net.py
+++ run_net.py
@@ -1,3 +1,5 @@
+from neural_network.network import Network
+
 import dataset_gen as data
 training, evaluation = data.generate_sets()
 
@@ -5,6 +7,10 @@ training, evaluation = data.generate_sets()
 sample = next(training())
 n_pix = sample.shape[0]*sample.shape[1]
 n_nodes = [n_pix, n_pix]
+
+autoencoder = Network(model=None)
+autoencoder.train(training)
+autoencoder.train(evaluation)
 
 # Get infinite dataaaa
 while 1:
